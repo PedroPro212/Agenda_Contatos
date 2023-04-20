69c0d68079a5363dedf8e3831c9496d05ff68708
Pedro Oliveira <96023606+PedroPro212@users.noreply.github.com>
2023-04-20 13:19:08 -0300

    Mostrar nome e sobrenome juntos

--- script/new.js
+++ script/new.js
@@ -1,3 +1,4 @@
+// Função para criar um novo contato
 function New(){
     var conteudo = document.querySelector('#contatos');
     var criar = document.querySelector('#criar');
@@ -7,6 +8,7 @@ function New(){
 
 }
 
+// Função para cancelar a criação de um novo contato
 function Cancelar(){
     var conteudo = document.querySelector('#contatos');
     var criar = document.querySelector('#criar');
@@ -14,3 +16,32 @@ function Cancelar(){
     conteudo.style.display = 'block';
     criar.style.display = 'none';
 }
+
+function Image(){
+    const inputImagem = document.getElementById("imagem");
+    const labelImagem = document.querySelector(".imagem");
+    
+    // adicionar um listener de mudança de arquivo
+    inputImagem.addEventListener("change", function() {
+      // criar um objeto FileReader
+      const leitorArquivo = new FileReader();
+    
+      // definir o que fazer quando o leitor ler o arquivo
+      leitorArquivo.addEventListener("load", function() {
+        // definir o valor da propriedade "background-image" da label como a URL da imagem
+        labelImagem.style.backgroundImage = "url("+leitorArquivo.result+")";
+      });
+    
+      // ler o arquivo selecionado pelo usuário
+      leitorArquivo.readAsDataURL(inputImagem.files[0]);
+    });
+}
+
+function Nome(){
+    var completo = document.querySelector('#nome-completo');
+    var nome = document.querySelector('#nome').value;
+    var sobrenome = document.querySelector('#sobrenome').value;
+
+    completo.textContent = nome + ' ' + sobrenome;
+}
+
